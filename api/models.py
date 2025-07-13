6980058b5fe657833a71e21fc75ece6554a5f8d5
GSGFs <gsgfs@gsgfs.moe>
2025-07-13 20:41:39 +0800

    feat: enhance Post model with validation, metadata extraction, and slug generation; update related models and signals

--- api/models.py
+++ api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils.text import Truncator, slugify
 
-from .utils import extract_keywords
+from .utils import chinese_slugify, extract_metadata
 
 
 class BaseModel(models.Model):
@@ -21,14 +21,14 @@ class BaseModel(models.Model):
 
 
 class Tag(BaseModel):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True, db_index=True, null=False)
 
     def __str__(self):
         return self.name
 
 
 class Category(BaseModel):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True, db_index=True, null=False)
 
     def __str__(self):
         return self.name
@@ -48,10 +48,14 @@ class Gal(BaseModel):
     vndb_rating = models.FloatField(blank=True, null=True)
 
     # review
-    summary = models.CharField(max_length=200, blank=True, null=True)  # No spoilers
+    summary = models.CharField(
+        max_length=200, blank=True, null=True, help_text="显示在外面, 类似于备注"
+    )  # No spoilers
     review = models.TextField(blank=True, null=True)
 
-    cover_image = models.URLField(max_length=500, blank=True, null=True)
+    cover_image = models.URLField(
+        max_length=500, blank=True, null=True, help_text="(暂时没用)"
+    )
 
     def __str__(self):
         return self.vndb_id
@@ -62,38 +66,46 @@ class Gal(BaseModel):
 
 class Post(BaseModel):
     # 基础信息
-    title = models.CharField(max_length=50)
-    content = models.TextField()
+    title = models.CharField(
+        max_length=50, unique=True, db_index=True, null=False, blank=True
+    )
+    content = models.TextField(blank=False, null=False, help_text="文章正文, 必填")
 
     # 渲染后的内容
-    content_html = models.TextField(null=True, blank=True)
+    content_html = models.TextField(null=True, blank=True, help_text="(暂时没用)")
 
     # 图片相关
     cover_image = models.URLField(
         max_length=500, blank=True, null=True, help_text="文章封面图片(URL)"
     )
     header_image = models.URLField(
-        max_length=500, blank=True, null=True, help_text="文章顶部大图(URL)"
-    )
+        max_length=500, blank=True, null=True, help_text="文章og显示图片(URL)"
+    )  # The only purpose now is as og image in website metadata
 
     # SEO相关
     slug = models.SlugField(
-        max_length=100, unique=True, blank=True, help_text="url 地址", db_index=True
+        max_length=100,
+        unique=True,
+        blank=True,
+        help_text="url 地址 (暂时没用)",
+        db_index=True,
     )
     meta_description = models.CharField(
         max_length=160,
         blank=True,
-        help_text="SEO描述, 留空将自动生成",
+        help_text="SEO描述",
     )
     keywords = models.CharField(
         max_length=200, blank=True, help_text="文章关键词，用逗号分隔"
     )
 
     # 统计
-    view_count = models.PositiveIntegerField(default=0)
+    view_count = models.PositiveIntegerField(default=0, help_text="(暂时没用)")
 
     # 排序
-    order = models.IntegerField(default=0, help_text="是否覆盖默认的优先级")
+    order = models.IntegerField(
+        default=0, help_text="是否覆盖默认的优先级, 数值越大越靠前"
+    )
 
     # 查询相关
     tags = models.ManyToManyField(
@@ -101,7 +113,7 @@ class Post(BaseModel):
         blank=True,
         # null=True, 本来就可以为空
         default=None,
-        related_name="post",
+        related_name="posts",
     )  # tags 可以对多
 
     category = models.ForeignKey(
@@ -110,7 +122,7 @@ class Post(BaseModel):
         blank=True,  # 在表单中可以为空
         null=True,  # 在数据库中可以为空
         default=None,
-        related_name="post",
+        related_name="posts",
     )  # 只能有一个 禁止删除还有文章的分类
 
     # 文章状态
@@ -128,18 +140,52 @@ class Post(BaseModel):
     def __str__(self):
         return self.title
 
-    # 重写保存时的操作
+    # rewrite save action
+    # called after adminFrom (at `./admin.py`)
     def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
+        post_metadata = extract_metadata(self.content)
 
+        # === title ===
+        if not self.title:
+            self.title = post_metadata.get("title") or ""
+
+        # === slug ===
+        if not self.slug:
+            self.slug = post_metadata.get("slug")
+        if not self.slug and self.title:
+            self.slug = chinese_slugify(self.title)
+
+        # === description ===
         if not self.meta_description:
             self.meta_description = Truncator(self.content).chars(150, html=True)[:160]
 
+        # === keywords ===
         if not self.keywords:
-            self.keywords = extract_keywords(self.content)
+            self.keywords = post_metadata.get("keywords")
 
-        return super().save(*args, **kwargs)
+        # === image ===
+        if not self.cover_image:
+            self.cover_image = post_metadata.get("cover_image")
+        if not self.header_image:
+            self.header_image = post_metadata.get("header_image")
+
+        super().save(*args, **kwargs)
+
+        # === tags ===
+        if not self.tags.exists():  # must use `.exists()` to check it
+            tags_to_set = []
+            tag_list = post_metadata["tags"]
+            for tag_name in tag_list:
+                tag_obj, created = Tag.objects.get_or_create(name=tag_name)
+                tags_to_set.append(tag_obj)
+            self.tags.set(tags_to_set)
+
+        # === category ===
+        if not self.category and post_metadata.get("category"):
+            category = post_metadata.get("category")
+            category, created = Category.objects.get_or_create(name=category)
+            self.category = category
+            super().save(update_fields=["category"])
 
 
 class Page(BaseModel):
@@ -220,7 +266,7 @@ class Page(BaseModel):
             self.meta_description = Truncator(self.content).chars(150, html=True)[:160]
 
         if not self.keywords:
-            self.keywords = extract_keywords(self.content)
+            self.keywords = extract_metadata(self.content).get("keywords")
 
         return super().save(*args, **kwargs)
 
@@ -249,8 +295,8 @@ class Guest(BaseModel):
 
 class Comment(BaseModel):
     content = models.TextField(max_length=10000)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comment")
-    guest = models.ForeignKey(Guest, on_delete=models.CASCADE, related_name="comment")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    guest = models.ForeignKey(Guest, on_delete=models.CASCADE, related_name="comments")
 
     # 用户信息
     user_agent = models.CharField(max_length=100, blank=True, default="unknown")
@@ -275,7 +321,8 @@ class Anime(BaseModel):
     # https://en.wikipedia.org/wiki/Motion_Picture_Association_film_rating_system
     rating = models.CharField(blank=True, null=True)
 
-    score = models.FloatField(blank=True, null=True)  # fill it by your self
+    # fill it by your self
+    score = models.FloatField(blank=True, null=True)
     review = models.TextField(blank=True, null=True)
 
     class Meta(BaseModel.Meta):
--- api/signals.py
+++ api/signals.py
@@ -2,16 +2,15 @@ from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
 from .jikan import query_anime
-from .models import Anime, Gal, Page, Post
-from .utils import extract_keywords
+from .models import Anime, Gal
 from .vndb import query_vn
 
-
-@receiver(pre_save, sender=Post)
-@receiver(pre_save, sender=Page)
-def add_keywords(sender, instance, **kwargs):
-    if not instance.keywords:
-        instance.keywords = extract_keywords(instance.content)
+# Post.save() already updated keywords
+# @receiver(pre_save, sender=Post)
+# @receiver(pre_save, sender=Page)
+# def add_keywords(sender, instance, **kwargs):
+#     if not instance.keywords:
+#         instance.keywords = extract_metadata(instance.content).get("keywords")
 
 
 @receiver(pre_save, sender=Gal)
