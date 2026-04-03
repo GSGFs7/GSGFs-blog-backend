c6c9f6485f6039d5dc788c51c9b9f1a1377dbb4a
GSGFs <gsgfs@gsgfs.moe>
2026-04-03 15:14:14 +0800

    feat: rate_limit sync support

--- api/rate_limit.py
+++ api/rate_limit.py
@@ -2,25 +2,31 @@ from functools import wraps
 from typing import Callable
 
 from django.core.cache import cache
+from django.http import HttpRequest
 
 from api.utils import is_async
+
+__all__ = ["rate_limit"]
+
+
+def _get_client_ip(request: HttpRequest) -> str:
+    client_ip = request.META.get("HTTP_X_FORWARDED_FOR")
+    if client_ip:
+        return client_ip.split(",")[0]
+    return request.META.get("REMOTE_ADDR")
 
 
 def rate_limit(key_prefix: str, max_requests: int, window: int):
     def decorator(func: Callable):
         @wraps(func)
-        async def wrapper(request, *args, **kwargs):
+        async def async_wrapper(request: HttpRequest, *args, **kwargs):
             # get ip
-            client_ip: str = request.META.get("HTTP_X_FORWARDED_FOR")
-            if client_ip:
-                client_ip = client_ip.split(",")[0]
-            else:
-                client_ip = request.META.get("REMOTE_ADDR")
+            client_ip = _get_client_ip(request)
 
             cache_key = f"rate_limit:{key_prefix}:{client_ip}"
             await cache.aadd(cache_key, 0, timeout=window)
             current_requests = await cache.aincr(cache_key)
-            if current_requests >= max_requests:
+            if current_requests > max_requests:
                 return 429, {"message": "Too many request"}
 
             # run the raw func
@@ -28,6 +34,22 @@ def rate_limit(key_prefix: str, max_requests: int, window: int):
                 return await func(request, *args, **kwargs)
             return func(request, *args, **kwargs)
 
-        return wrapper
+        @wraps(func)
+        def sync_wrapper(request: HttpRequest, *args, **kwargs):
+            # get ip
+            client_ip = _get_client_ip(request)
+
+            cache_key = f"rate_limit:{key_prefix}:{client_ip}"
+            cache.add(cache_key, 0, timeout=window)
+            current_requests = cache.incr(cache_key)
+            if current_requests > max_requests:
+                return 429, {"message": "Too many request"}
+
+            # run the raw func
+            return func(request, *args, **kwargs)
+
+        if is_async(func):
+            return async_wrapper
+        return sync_wrapper
 
     return decorator
