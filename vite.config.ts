e6359c486821800fe87d6e612706237c02e2aeea
GSGFs <gsgfs@gsgfs.moe>
2026-04-17 16:01:44 +0800

    fix: vite plugin watching files

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,26 @@
 import tailwindcss from "@tailwindcss/vite";
+import type { Plugin } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import { defineConfig } from "vitest/config";
-import { HmrContext } from "vite";
 
-function djangoTemplateReload() {
-  return {
-    name: "django-template-reload",
-    handleHotUpdate: (ctx: HmrContext) => {
-      // merge this?
-      const templateDirs = ["web/templates/", "templates/"];
-      if (ctx.file.endsWith(".html") && templateDirs.some((dir) => ctx.file.includes(dir))) {
-        ctx.server.ws.send({ type: "full-reload" });
-        return [];
+// reload entre page when django template has updated
+const djangoTemplateReload = (): Plugin => ({
+  name: "django-template-reload",
+  configureServer: (server) => {
+    const templateDirs = ["web/templates/", "templates/"];
+    console.log(templateDirs);
+    server.watcher.add(templateDirs);
+
+    const reload = (path: string): void => {
+      if (path.endsWith(".html")) {
+        server.ws.send({ type: "full-reload" });
       }
-    },
-  };
-}
+    };
+    server.watcher.on("add", reload);
+    server.watcher.on("change", reload);
+    server.watcher.on("unlink", reload);
+  },
+});
 
 export default defineConfig(({ command }) => ({
   base: command === "build" ? "/static/dist/" : "/",
@@ -27,7 +32,7 @@ export default defineConfig(({ command }) => ({
     rolldownOptions: {
       input: {
         main: "web/typescript/index.tsx",
-        styles: "web/typescript/globals.css",
+        styles: "web/typescript/styles/globals.css",
         loadTheme: "web/typescript/load-theme.ts",
         htmx: "web/typescript/htmx.ts",
       },
